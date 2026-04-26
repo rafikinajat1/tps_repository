2de2a1705aa9bbec15cb03e28f7aa39523d35dca
rafikinajat1 <rafikinajat1@gmail.com>
2026-04-26 02:44:40 +0200

    Update Mammifere.java

---
+++ TP1/src/exercice1.1/Mammifere.java
@@ -0,0 +1,13 @@
+package exercice1.1;
+
+public class Mammifere extends Animal {
+
+    public Mammifere(String nom) {
+        super(nom);
+    }
+
+    @Override
+    public void faireDuBruit() {
+        System.out.println(getNom() + " grogne.");
+    }
+}
