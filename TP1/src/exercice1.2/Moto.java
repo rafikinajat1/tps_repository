b05ac5db4128f31d6a220fa1921d3fffc29aa327
rafikinajat1 <rafikinajat1@gmail.com>
2026-04-26 02:57:40 +0200

    Update Moto.java

--- TP1/src/exercice1.2/Moto.java
+++ TP1/src/exercice1.2/Moto.java
@@ -1,1 +1,20 @@
+package exercice1.2;
 
+public class Moto extends Vehicule {
+
+    private String typeDeGuidon;
+
+    public Moto(String marque, String modele, int annee, String typeDeGuidon) {
+        super(marque, modele, annee);
+        this.typeDeGuidon = typeDeGuidon;
+    }
+
+    @Override
+    public void afficherDetails() {
+        demarrer();
+        System.out.println(
+            "Moto: " + getMarque() + " " + getModele() +
+            " (" + getAnnee() + "), Guidon: " + typeDeGuidon
+        );
+    }
+}
