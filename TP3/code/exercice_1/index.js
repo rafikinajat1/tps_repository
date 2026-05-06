f1c407a7a67219bd1b6e26126d7cb910fa774955
Najat <rafikinajat1@example.com>
2026-05-06 02:33:56 +0200

    Doc: améliorer les commentaires de addition

--- TP3/code/exercice_1/index.js
+++ TP3/code/exercice_1/index.js
@@ -1,1 +1,1 @@
-function addition(a, b) { return a + b; } if (require.main === module) { console.log('Resultat:', addition(5, 3)); } module.exports = { addition }; 
+function addition(a, b) {return a + b; // simple addition}function soustraction(a, b) {return a - b;}if (require.main === module) {console.log("R‚sultat addition:", addition(5, 3));console.log("R‚sultat soustraction:", soustraction(5, 3));}module.exports = { addition, soustraction }; 
